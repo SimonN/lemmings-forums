0319f5694292c0ad178030cea8c4ff3db2916b81
Namida <namida.verasche@gmail.com>
2019-05-17 16:25:40 +1200

    Backup script fix

--- backup.sh
+++ backup.sh
@@ -15,18 +15,18 @@ tar -cf ../private/backup/attachments.tar attachments*/*
 tar -cf ../private/backup/pm-attachments.tar pm_attachments*/*
 
 cd ../private/backup/
-mysqldump -u $MYSQL_USERNAME -p$MYSQL_PASSWORD --databases LemForums > LemmingsForums_${DATESTAMP}.sql
+mysqldump -u $MYSQL_USERNAME -p$MYSQL_PASSWORD --databases LemForums --result-file=LemmingsForums_${DATESTAMP}.sql
 
-tar -czf LemmingsForums_${DATESTAMP}.sql.gz LemmingsForums_${DATESTAMP}.sql
+tar -czf LemmingsForums_${DATESTAMP}_Database.tar.gz LemmingsForums_${DATESTAMP}.sql
 rm LemmingsForums_${DATESTAMP}.sql
 
 tar -czf LemmingsForums_${DATESTAMP}_Attachments.tar.gz *.tar
 rm *.tar
 
-if ( rclone move LemmingsForums_${DATESTAMP}.sql.gz gdrive: ) &&
+if ( rclone move LemmingsForums_${DATESTAMP}_Database.tar.gz gdrive: ) &&
    ( rclone move LemmingsForums_${DATESTAMP}_Attachments.tar.gz gdrive: ); then
     if [ "${#BACKUP_DATES[@]}" -gt "${PRESERVE_BACKUP_COUNT}" ]; then
-      rclone delete gdrive:LemmingsForums_${BACKUP_DATES[0]}.sql.gz
+      rclone delete gdrive:LemmingsForums_${BACKUP_DATES[0]}_Database.tar.gz
       rclone delete gdrive:LemmingsForums_${BACKUP_DATES[0]}_Attachments.tar.gz
 
       BACKUP_DATES=( ${BACKUP_DATES[@]:1} )
