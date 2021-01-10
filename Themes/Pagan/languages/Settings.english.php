990be4a9da5b2982adf8ddfe57112c63aeb8f9dc
Namida Verasche <namida.verasche@gmail.com>
2021-01-11 09:09:03 +1300

    Selectable Lemmings vs Original theme.

--- Themes/Pagan/languages/Settings.english.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Version: 2.0; Settings
-
-$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
-$txt['theme_description'] = 'Pagan by <a href="http://www.bryandeakin.com">Runic</a>.';
-
-?>
